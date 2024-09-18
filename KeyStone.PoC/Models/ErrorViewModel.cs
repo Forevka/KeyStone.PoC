6c426c32d51d85a8c2001116eb718b93644a62fb
Forevka <me@forevka.dev>
2024-09-18 17:53:09 +0300

    init

---
+++ KeyStone.PoC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KeyStone.PoC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
